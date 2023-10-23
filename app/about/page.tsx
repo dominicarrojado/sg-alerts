8acb7a6935c0e83b98d2f86847e3b632ecc2061e
Dominic Arrojado <dominicarrojado@gmail.com>
2023-10-23 18:15:32 +0800

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -48,21 +48,29 @@ export default function About() {
       </Paragraph>
       <Paragraph>
         It started out as a personal project to help me monitor the appointment
-        booking slots for my Japan tourist visa application. I was frustrated
-        with having to constantly check the embassy&apos;s website for available
-        slots, so I created a service that could automatically monitor the
-        website and notify me when a slot became available.
+        booking slots for my Japan tourist visa application. I had little time
+        left to apply for my visa, and I was having trouble finding an available
+        appointment slot. I was frustrated with having to constantly check the
+        embassy&apos;s website for available slots and I didn&apos;t want to pay
+        the expensive agency fees just to secure a slot. So I created a service
+        that could automatically monitor the website and notify me when a slot
+        became available.
       </Paragraph>
       <Paragraph>
-        I knew that other people would find this service useful as well, so I
-        decided to share it with everyone. The service was well-received, and I
-        received many positive comments from people who found it useful.
+        I knew that other people would find the Japan Visa Appointment
+        Notification Service useful as well, so I decided to share it with
+        everyone. I created a simple website where people could subscribe to the
+        service and receive notifications for free. It was well received, and
+        many people were able to secure their visa appointment slots thanks to
+        the service.
       </Paragraph>
       <Paragraph>
-        After seeing how useful it was for other people, I decided to create SG
-        Alerts to expand the service to other topics as well. I have since added
-        more topics, and I&apos;m always looking for new topics to add. If you
-        have any suggestions, please feel free to{" "}
+        After seeing how useful it was, I decided to create{" "}
+        <span className="font-medium">SG Alerts</span> to expand the service to
+        other topics that are relevant to Singapore residents such as flight
+        prices, fixed deposit rates, driving centre appointment slots, and many
+        more. I&apos;m always looking for new topics to add. If you have any
+        suggestions or feedback, please feel free to{" "}
         <Anchor href={`mailto:${OWNER_EMAIL}`}>contact me</Anchor>.
       </Paragraph>
     </Container>
