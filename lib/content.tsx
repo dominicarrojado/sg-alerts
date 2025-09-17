f1b9618f93e6bb1aebe77d7d82a70177c2a6f6c3
Dominic Arrojado <dominicarrojado@gmail.com>
2025-09-17 22:35:01 +0800

    update donation testimonials

--- lib/content.tsx
+++ lib/content.tsx
@@ -1058,6 +1058,12 @@ export const TRAIN_SCHEDULE_ITEMS = [
 
 export const DONATION_TESTIMONIALS: DonationTestimonials = [
   {
+    firstName: "Trina",
+    lastName: "O",
+    message: "Thank you for saving me time and camping!",
+    date: "17 September 2025",
+  },
+  {
     firstName: "May",
     lastName: "L",
     message: "",
